f1b6644bb0cfd448f9727d56348c88354dac0321
MattSteinkamp <mdsteinkamp@gmail.com>
2024-07-24 16:10:46 -0700

    UserContext.js setting user so can refresh

--- reader_2/client/src/components/UserContext.js
+++ reader_2/client/src/components/UserContext.js
@@ -24,7 +24,7 @@ function UserProvider({ children }) {
             }
         }).then((resp) => {
             if (resp.ok) {
-                resp.json().then((user) => console.log(user))
+                resp.json().then((user) => setUser(user))
             } else {
                 console.log(resp)
             }
