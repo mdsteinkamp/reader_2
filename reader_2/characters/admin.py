6e5e250d5ac6764efde0696ee65d9c9194924611
Matthew Steinkamp <mdsteinkamp@gmail.com>
2024-08-07 14:54:31 -0700

    registered Character in admin site

--- reader_2/characters/admin.py
+++ reader_2/characters/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+class CahracterAdmin(admin.ModelAdmin):
+    list_display = (
+        "id", 
+        "name", 
+        "appearance", 
+        "locations",
+        "associates",
+        "position",
+        "knowledge"
+    )
+
+admin.site.register(models.Character, CahracterAdmin)
